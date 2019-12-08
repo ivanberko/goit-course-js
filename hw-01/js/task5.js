2d30381278183ec8a6fbb36de2dca85b8ab808e3
Ivan Berk <berkoivan@gmail.com>
2019-12-08 21:26:51 +0200

    changed task5

--- hw-01/js/task5.js
+++ hw-01/js/task5.js
@@ -4,39 +4,54 @@ let cost;
 let deliverProduct = prompt(
   'Укажите страну в которую хотите заказать доставку!',
 );
+let message5;
+
+if (deliverProduct === null) {
+  message5 = alert(`Доставка не выбрана`);
+}
 
 switch (deliverProduct.toLowerCase()) {
   case 'китай':
     cost = 100;
-    console.log(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
-    alert(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
+    message5 = alert(
+      `Доставка в ${deliverProduct} будет стоить ${cost} кредитов`,
+    );
     break;
 
   case 'чили':
     cost = 250;
-    console.log(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
-    alert(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
+    message5 = alert(
+      `Доставка в ${deliverProduct} будет стоить ${cost} кредитов`,
+    );
+
     break;
 
   case 'австралия':
     cost = 170;
-    console.log(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
-    alert(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
+    message5 = alert(
+      `Доставка в ${deliverProduct} будет стоить ${cost} кредитов`,
+    );
+
     break;
 
   case 'индия':
     cost = 80;
-    console.log(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
-    alert(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
+    message5 = alert(
+      `Доставка в ${deliverProduct} будет стоить ${cost} кредитов`,
+    );
+
     break;
 
   case 'ямайка':
     cost = 120;
-    console.log(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
-    alert(`Доставка в ${deliverProduct} будет стоить ${cost} кредитов`);
+    message5 = alert(
+      `Доставка в ${deliverProduct} будет стоить ${cost} кредитов`,
+    );
+
     break;
 
   default:
-    console.log('В вашей стране доставка не доступна');
-    alert('В вашей стране доставка не доступна');
+    message5 = alert(`В вашей стране доставка не доступнв`);
 }
+
+console.log(message5);
