6a0081b58ecae3e0c5b6fa4af2e6644455a4f391
Ivan Berk <berkoivan@gmail.com>
2020-01-19 00:25:05 +0200

    added task-6

---
+++ hw-02/js/task6.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+  input = prompt(`Add number`);
+
+  if (Number.isNaN(Number(input)) || input === '') {
+    alert('Введено не корректное значение');
+    continue;
+  } else if (input >= 0 && input !== null) {
+    numbers.push(Number(input));
+  }
+} while (input !== null);
+console.log(numbers);
+
+if (numbers.length !== 0) {
+  for (const number of numbers) {
+    total += number;
+    alert (`Общая сумма чисел равна ${total}`);
+  }
+}
+
+console.log(total);
