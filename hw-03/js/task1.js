3c23860c3d045194964bc7c56da809bb73a1ab4a
Ivan Berk <berkoivan@gmail.com>
2020-01-22 22:32:25 +0200

    added task1 in hw-03

--- hw-03/js/task1.js
+++ hw-03/js/task1.js
@@ -1,1 +1,22 @@
 'use strict';
+
+console.log('TASK1------------------------')
+
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const showObject = objKeys => {
+  for (const key of objKeys) {
+    console.log(`${key}: ${user[key]}`);
+  }
+};
+
+showObject(Object.keys(user));
