c9e3f2db37e2f9898030cae01a858e8f6d191ee3
Ivan Berk <berkoivan@gmail.com>
2020-01-23 00:28:46 +0200

    added task2, task3 in hw-03

--- hw-03/js/task2.js
+++ hw-03/js/task2.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log('TASK2------------------------')
+
+const countProps = obj => {
+ return Object.keys(obj).length;
+}
+
+console.log(countProps({}));
+console.log(countProps({ name: 'Mango', age: 2 }));
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
--- hw-03/js/task3.js
+++ hw-03/js/task3.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const findBestEmployee = employees => {
+  let bestEmployee;
+  let max = 0;
+  for (const key in employees) {
+    if (employees[key] > max) {
+      max = employees[key];
+      bestEmployee = key;
+    }
+  }
+  return bestEmployee;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+);
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+);
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+);
