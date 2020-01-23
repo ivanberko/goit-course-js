7d0a69655d52120838d0b71f4dc67e5cb14f5c69
Ivan Berk <berkoivan@gmail.com>
2020-01-24 00:18:48 +0200

    added task4 in hw-03

--- hw-03/js/task4.js
+++ hw-03/js/task4.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const countTotalSalary = function(employees) {
+  let sum = 0;
+  for (const key in employees) {
+    sum += employees[key];
+  }
+  return sum;
+};
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+);
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+);
