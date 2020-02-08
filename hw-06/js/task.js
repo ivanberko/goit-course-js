4ae6e6a1c3148cfebdb1546a0e2cf61c0cb951ce
Ivan Berk <berkoivan@gmail.com>
2020-02-08 19:43:32 +0200

    corrected task10

--- hw-06/js/task.js
+++ hw-06/js/task.js
@@ -82,8 +82,10 @@ console.log(getNamesSortedByFriendsCount(users));
 console.log('TASK10---------------------------------------------------');
 //Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке
 const getSortedUniqueSkills = users => {
-  const skillsAllUsers = users.flatMap(user => user.skills).sort();
-  return skillsAllUsers.filter((el, i) => skillsAllUsers.indexOf(el) === i);
+  return users
+    .flatMap(user => user.skills)
+    .filter((el, i, arr) => arr.indexOf(el) === i)
+    .sort();
 };
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
