d037c93b6b93c2625c189e90c77f068e47bb5857
Ivan Berk <berkoivan@gmail.com>
2020-03-10 23:51:10 +0200

    added opening and closing modal gallery in hw-08

--- hw-08/task.js
+++ hw-08/task.js
@@ -2,23 +2,51 @@
 import gallery from './gallery-items.js';
 
 const galleryList = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxImg = document.querySelector('.lightbox__image');
+// const closeLightboxBtn = document.querySelector(
+//   'button[data-action="close-lightbox"]',
+// );
 
-const galleryItems = function(array) {
-  return array.map((elem) => {
-    const item = document.createElement('li');
-    item.classList.add('gallery__item');
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.setAttribute('href', elem.original);
-    const img = document.createElement('img');
-    img.classList.add('gallery__image');
-    img.setAttribute('src', elem.preview);
-    img.setAttribute('alt', elem.description);
-    img.dataset.source = elem.original;
-    link.append(img);
-    item.append(link);
-    return item;
-  });
+const galleryItems = gallery.map((elem) => {
+  const item = document.createElement('li');
+  item.classList.add('gallery__item');
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.setAttribute('href', elem.original);
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.setAttribute('src', elem.preview);
+  img.setAttribute('alt', elem.description);
+  img.dataset.source = elem.original;
+  link.append(img);
+  item.append(link);
+  return item;
+});
+
+galleryList.append(...galleryItems);
+
+const openModal = function(event) {
+  if (event.target.tagName === 'IMG') {
+    event.preventDefault();
+    lightboxImg.src = event.target.dataset.source;
+    lightbox.classList.add('is-open');
+  }
 };
 
-galleryList.append(...galleryItems(gallery));
+const closeModal = function(event) {
+  if (event.target.tagName === 'IMG') {
+    return;
+  }
+  lightboxImg.removeAttribute('src');
+  lightbox.classList.remove('is-open');
+};
+
+galleryList.addEventListener('click', openModal);
+lightbox.addEventListener('click', closeModal);
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Escape') {
+    lightboxImg.removeAttribute('src');
+    lightbox.classList.remove('is-open');
+  }
+});
