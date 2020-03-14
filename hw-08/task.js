b86baaca1ab460be894ffe95b781bb269f8775b5
Ivan Berk <berkoivan@gmail.com>
2020-03-14 22:23:12 +0200

    Added scrolling modal to keys - done

--- hw-08/task.js
+++ hw-08/task.js
@@ -8,14 +8,17 @@ const lightboxImg = document.querySelector('.lightbox__image');
 const createGalleryItems = gallery.map((elem) => {
   const item = document.createElement('li');
   item.classList.add('gallery__item');
+
   const link = document.createElement('a');
   link.classList.add('gallery__link');
   link.setAttribute('href', elem.original);
+
   const img = document.createElement('img');
   img.classList.add('gallery__image');
   img.setAttribute('src', elem.preview);
   img.setAttribute('alt', elem.description);
   img.dataset.source = elem.original;
+
   link.append(img);
   item.append(link);
   return item;
@@ -24,31 +27,38 @@ const createGalleryItems = gallery.map((elem) => {
 galleryList.append(...createGalleryItems);
 
 const handleKeyUp = function(event) {
-  if (event.code === 'Escape') {
-    lightboxImg.removeAttribute('src');
-    lightbox.classList.remove('is-open');
-  }
+  switch (event.code) {
+    case 'Escape':
+      lightboxImg.removeAttribute('src');
+      lightbox.classList.remove('is-open');
+      break;
 
-  if (event.code === 'ArrowRight') {
-    for (let i = 0; i < gallery.length; i += 1) {
-      if (lightboxImg.src === gallery[i].original) {
-        lightboxImg.src = gallery[(i += 1)].original;
-        // if (lightboxImg.src === gallery[gallery.length - 1].original) {
-        //   return;
-        // }
+    case 'ArrowRight': {
+      const next = gallery.findIndex(
+        (item) => lightboxImg.src === item.original,
+      );
+      if (next + 1 < gallery.length) {
+        lightboxImg.src = gallery[next + 1].original;
+        return;
       }
+      lightboxImg.src = gallery[0].original;
+      break;
     }
-  }
 
-  if (event.code === 'ArrowLeft') {
-    for (let i = 0; i < gallery.length; i += 1) {
-      if (lightboxImg.src === gallery[i].original) {
-        lightboxImg.src = gallery[(i -= 1)].original;
-        // if (lightboxImg.src === gallery[0].original) {
-        //   lightboxImg.src = gallery[gallery.length - 1].original;
-        // }
+    case 'ArrowLeft': {
+      const prev = gallery.findIndex(
+        (item) => lightboxImg.src === item.original,
+      );
+      if (prev - 1 >= 0) {
+        lightboxImg.src = gallery[prev - 1].original;
+        return;
       }
+      lightboxImg.src = gallery[gallery.length - 1].original;
+      break;
     }
+
+    default:
+      break;
   }
 };
 
