beb061a3b61ac3aef217acc8c7bb54ed5533a0b6
Ivan Berk <berkoivan@gmail.com>
2020-03-29 22:04:11 +0300

    init hw-12

---
+++ hw-12/src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import axios from 'axios';
+import { v4 } from 'uuid';
+import debounce from 'lodash.debounce';
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons.js';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+
+// PNotify.error({
+//   text: 'Too many matches found. Please enter a more specific query!',
+// });
